8dde61e42077a921b4e98d57966fe4b447289b47
Sebastian Jakubowski <sebastianjakubowski22@gmail.com>
2025-01-24 10:10:01 +0100

    Dodanie powracania do menu wyboru po dokonaniu wyboru lub obliczenia

--- obliczenia.c
+++ obliczenia.c
@@ -12,38 +12,41 @@ float sphere_volume(float r) {
 int main() {
     int choice;
 
-    printf("Wybierz bryle, dla ktorej chcesz obliczyæ objetosc:\n");
-    printf("1. Prostopadloscian\n");
-    printf("2. Kula\n");
-    printf("0. Zakoncz\n");
-    printf("Twoj wybor: ");
-    scanf("%d", &choice);
+    while (1) {
+        printf("\nWybierz bryle, dla ktorej chcesz obliczyc objetosc:\n");
+        printf("1. Prostopadloscian\n");
+        printf("2. Kula\n");
+        printf("0. Zakoncz\n");
+        printf("Twoj wybor: ");
+        scanf("%d", &choice);
 
-    if (choice == 1) {
-        float a, b, h;
-        printf("Podaj dlugosc prostopadloscianu: ");
-        scanf("%f", &a);
+        if (choice == 1) {
+            float a, b, h;
+            printf("Podaj dlugosc prostopadloscianu: ");
+            scanf("%f", &a);
 
-        printf("Podaj szerokosc prostopadloscianu: ");
-        scanf("%f", &b);
+            printf("Podaj szerokosc prostopadloscianu: ");
+            scanf("%f", &b);
 
-        printf("Podaj wysokosc prostopadloscianu: ");
-        scanf("%f", &h);
+            printf("Podaj wysokosc prostopadloscianu: ");
+            scanf("%f", &h);
 
-        printf("\nObjetosc prostopadloscianu: %f\n", cuboid_volume(a, b, h));
-    }
-    else if (choice == 2) {
-        float r;
-        printf("Podaj promien kuli: ");
-        scanf("%f", &r);
+            printf("\nObjetosc prostopadloscianu: %f\n", cuboid_volume(a, b, h));
+        }
+        else if (choice == 2) {
+            float r;
+            printf("Podaj promien kuli: ");
+            scanf("%f", &r);
 
-        printf("\nObjetosc kuli: %f\n", sphere_volume(r));
-    }
-    else if (choice == 0) {
-        printf("Program zostaje zakonczony.\n");
-    }
-    else {
-        printf("Nieprawid³owy wybor. Proszê wybraa 1, 2 lub 0.\n");
+            printf("\nObjetosc kuli: %f\n", sphere_volume(r));
+        }
+        else if (choice == 0) {
+            printf("Program zostaje zakonczony.\n");
+            break;
+        }
+        else {
+            printf("Nieprawidlowy wybor. Prosze wybrac 1, 2 lub 0.\n");
+        }
     }
 
     return 0;
