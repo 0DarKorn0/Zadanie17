2d40675e2deec85774f87c79f745488a1c44f4ca
Sebastian Jakubowski <sebastianjakubowski22@gmail.com>
2025-01-24 09:36:15 +0100

    Obliczanie objętości kuli | zakomentowanie obliczania objętości prostopadłościanu

--- obliczenia.c
+++ obliczenia.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <math.h>
+/*
 float cuboid_volume(float a, float b, float h) {
     return (a*b*h);
 }
@@ -17,4 +18,17 @@ int main(){
     scanf("%f", &h);
 
     printf("\n Objetosc prostopadloscianu: %f \n", cuboid_volume(a, b, h));
+}
+*/
+
+float sphere_volume(float r){
+    return((4.0/3.0)*3.14*(r*r*r));
+}
+
+int main(){
+    float r;
+    printf("Podaj promien kuli: ");
+    scanf("%f", &r);
+
+    printf("\n Objetosc kuli: %f \n", sphere_volume(r));
 }
